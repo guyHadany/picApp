52376185649083494b3f5a721dbf4b5de7c109b0
Noam katz <nuna@Noams-MacBook-Pro.local>
2019-07-23 19:04:49 +0300

    finished get route

--- dist/renderer.js
+++ dist/renderer.js
@@ -1,12 +1,24 @@
 class Render {
 
-    renderData(data) {
+    renderData(unsplash, pexels, pixabay) {
         $('.unsplashBox').empty()
+        $('.pexelsBox').empty()
+        $('.pixabayBox').empty()
 
         const source = $('#unsplash-template').html();
         const template = Handlebars.compile(source);
-        const newHTML = template({data});
+        const newHTML = template({unsplash});
         $('.unsplashBox').append(newHTML);
+
+        const source = $('#pexels-template').html();
+        const template = Handlebars.compile(source);
+        const newHTML = template({pexels});
+        $('.pexelsBox').append(newHTML);
+
+        const source = $('#pixabay-template').html();
+        const template = Handlebars.compile(source);
+        const newHTML = template({pixabay});
+        $('.pixabayBox').append(newHTML);
     }
 
 
