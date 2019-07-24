f6c8eb42b77daf86d29a66ce862415582e89e359
Hadar Alon <hadaralon3@gmail.com>
2019-07-24 20:35:48 +0300

    ok

--- dist/pictureManager.js
+++ dist/pictureManager.js
@@ -14,10 +14,19 @@ class PicManager {
     }
     //Loop trough unsplashPics array to match id's (with the one you pressed) add passes the id as an argument to the pic POST routh to be saved in the DB 
     savePic(picId, projectName) {
-        const picInfo = this.unsplashPics.find(p => p.apiId == picId)
+        let picInfo 
+        if (this.unsplashPics.find(p => p.apiId == picId)) {
+            picInfo = this.unsplashPics.find(p => p.apiId == picId)
+        } else if (this.pexelsPics.find(p => p.apiId == picId)) {
+            picInfo = this.pexelsPics.find(p => p.apiId == picId)
+        } else {
+            picInfo = this.pixabayPics.find(p => p.apiId == picId)
+        }
+  
         picInfo.isSaved = true
         $.post(`/pic/${projectName}`, picInfo)
     }
+
   // makes a GET request to the DBpics routh to get the saved pics from the DB - fills savedPics array & clear all the other arrays 
     async getPicsFromDB() {
         let PicsFromDB = await $.get(`/DBpics`)
@@ -34,4 +43,37 @@ class PicManager {
         })
         await this.getPicsFromDB()
     }
+
+
+
+    async getProjectName() {
+      let names = await $.get('/projectsNames')
+      console.log(names);
+      
+       return names
+    }
+
+
+
+     addToProject(name, picId) {
+        let picInfo 
+
+        if (this.unsplashPics.find(p => p.apiId == picId)) {
+            picInfo = this.unsplashPics.find(p => p.apiId == picId)
+        } else if (this.pexelsPics.find(p => p.apiId == picId)) {
+            picInfo = this.pexelsPics.find(p => p.apiId == picId)
+        } else {
+            picInfo = this.pixabayPics.find(p => p.apiId == picId)
+        }
+        
+        picInfo.isSaved = true
+        $.post(`/projects/${name}`, picInfo )
+
+        
+    }
+
+
+
+
+
 }
